d76778fb560fa995ff399d6944bb35eff2267e94
19521592 <38987479+19521592@users.noreply.github.com>
2021-04-05 13:26:38 +0700

    add recipe list in user profile

--- lib/screens/user_profile_screen.dart
+++ lib/screens/user_profile_screen.dart
@@ -1,8 +1,10 @@
+import 'package:cooking_master/models/recipe_card_model.dart';
 import 'package:cooking_master/models/user_model.dart';
 import 'package:cooking_master/screens/UserProfile/category_item.dart';
 import 'package:cooking_master/screens/edit_user_profile_screen.dart';
 import 'package:cooking_master/services/auth.dart';
 import 'package:cooking_master/widgets/appbar.dart';
+import 'package:cooking_master/widgets/recipe_detail_card.dart';
 import 'package:cooking_master/widgets/show_alert_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:cooking_master/widgets/CustomBackButton.dart';
@@ -56,7 +58,9 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             Navigator.push(
                 context,
                 MaterialPageRoute(
-                    builder: (context) => EditUserProfileScreen(user: user,)));
+                    builder: (context) => EditUserProfileScreen(
+                          user: user,
+                        )));
           },
         ),
         IconButton(
@@ -80,136 +84,154 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
       )),
       body: Container(
         padding: EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Center(
-              child: Hero(
-                tag: "avatar",
+        child: SingleChildScrollView(
+          physics: ScrollPhysics(),
+          child: Column(
+            children: [
+              Center(
+                child: Hero(
+                  tag: "avatar",
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        image:
+                            DecorationImage(image: AssetImage(user.userImage))),
+                  ),
+                ),
+              ),
+              Center(
                 child: Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      image:
-                          DecorationImage(image: AssetImage(user.userImage))),
-                ),
-              ),
-            ),
-            Center(
-              child: Container(
-                margin: EdgeInsets.only(top: 20),
-                child: Hero(
-                  tag: "username",
-                  child: Text(
-                    user.userName,
-                    style: GoogleFonts.roboto(
-                        color: Colors.black,
-                        fontSize: 21,
-                        fontWeight: FontWeight.w500),
-                  ),
-                ),
-              ),
-            ),
-            Center(
-              child: Container(
-                margin: EdgeInsets.only(top: 15),
-                child: Hero(
-                  tag: "useraddress",
-                  child: Text(
-                    user.userAddress,
-                    style: GoogleFonts.roboto(
-                        color: Colors.black,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w400),
-                  ),
-                ),
-              ),
-            ),
-            Center(
-              child: Container(
-                margin: EdgeInsets.only(top: 15),
-                child: IntrinsicHeight(
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              "Người theo dõi",
-                              style: GoogleFonts.roboto(
-                                  color: Colors.black,
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w400,
-                                  letterSpacing: 0.5,
-                                  height: 1.5),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              user.userFollowed.toString(),
-                              style: GoogleFonts.roboto(
-                                  color: Colors.black,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500),
-                            ),
-                          ],
-                        ),
-                        VerticalDivider(
+                  margin: EdgeInsets.only(top: 20),
+                  child: Hero(
+                    tag: "username",
+                    child: Text(
+                      user.userName,
+                      style: GoogleFonts.roboto(
                           color: Colors.black,
-                          thickness: 0.5,
-                          width: 20,
-                        ),
-                        Column(
-                          children: [
-                            Text(
-                              "Đang theo dõi",
-                              style: GoogleFonts.roboto(
-                                  color: Colors.black,
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w400,
-                                  letterSpacing: 0.5,
-                                  height: 1.5),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              user.userFollowing.toString(),
-                              style: GoogleFonts.roboto(
-                                  color: Colors.black,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500),
-                            ),
-                          ],
-                        ),
-                      ]),
-                ),
-              ),
-            ),
-            Center(
-              child: Container(
-                margin: EdgeInsets.only(top: 25),
-                padding: EdgeInsets.only(left: 25, right: 25),
-                child: Hero(
-                  tag: "userbio",
-                  child: Text(
-                    user.userBio,
-                    style: GoogleFonts.roboto(
-                        color: Colors.black,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w400,
-                        letterSpacing: 0.5,
-                        height: 1.5),
-                    textAlign: TextAlign.center,
+                          fontSize: 21,
+                          fontWeight: FontWeight.w500),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Center(
-              child: CategoryItem(),
-            ),
-          ],
+              Center(
+                child: Container(
+                  margin: EdgeInsets.only(top: 15),
+                  child: Hero(
+                    tag: "useraddress",
+                    child: Text(
+                      user.userAddress,
+                      style: GoogleFonts.roboto(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  margin: EdgeInsets.only(top: 15),
+                  child: IntrinsicHeight(
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                "Người theo dõi",
+                                style: GoogleFonts.roboto(
+                                    color: Colors.black,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w400,
+                                    letterSpacing: 0.5,
+                                    height: 1.5),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                user.userFollowed.toString(),
+                                style: GoogleFonts.roboto(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ],
+                          ),
+                          VerticalDivider(
+                            color: Colors.black,
+                            thickness: 0.5,
+                            width: 20,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "Đang theo dõi",
+                                style: GoogleFonts.roboto(
+                                    color: Colors.black,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w400,
+                                    letterSpacing: 0.5,
+                                    height: 1.5),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                user.userFollowing.toString(),
+                                style: GoogleFonts.roboto(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ],
+                          ),
+                        ]),
+                  ),
+                ),
+              ),
+              //Bio Text
+              Center(
+                child: Container(
+                  margin: EdgeInsets.only(top: 25),
+                  padding: EdgeInsets.only(left: 25, right: 25),
+                  child: Hero(
+                    tag: "userbio",
+                    child: Text(
+                      user.userBio,
+                      style: GoogleFonts.roboto(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0.5,
+                          height: 1.5),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: CategoryItem(),
+              ),
+              //List recipes
+              Container(
+                child: ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: cards.length,
+                  itemBuilder: (context, index) {
+                    return RecipeDetailCard(
+                      recipe: cards[index],
+                      size: MediaQuery.of(context).size,
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/ListCards/list_view_of_recipe_cards.dart
+++ lib/widgets/ListCards/list_view_of_recipe_cards.dart
@@ -18,31 +18,34 @@ class ListViewOfRecipeCards extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(top: defaultPadding*0.6),
+      padding: EdgeInsets.only(top: defaultPadding * 0.6),
       height: size.height * 0.35,
 
       //List of recipe cards
       child: ListView.builder(
           scrollDirection: Axis.horizontal,
-          padding: EdgeInsets.only(left: defaultPadding, right: defaultPadding*0.4),
+          padding: EdgeInsets.only(
+              left: defaultPadding, right: defaultPadding * 0.4),
           itemCount: cards.length,
-          itemBuilder: (context, index){
+          itemBuilder: (context, index) {
             return Container(
-              width: size.width* 0.5,
+              width: size.width * 0.5,
+              height: size.width * 0.5,
               margin: EdgeInsets.only(right: defaultPadding * 0.7),
               decoration: BoxDecoration(
-                  boxShadow: [BoxShadow(
-                    color: blue2.withOpacity(0.2),
-                    spreadRadius: 2,
-                    blurRadius: 6,
-                    offset: Offset(0, 3),
-                  )],
+                  boxShadow: [
+                    BoxShadow(
+                      color: blue2.withOpacity(0.2),
+                      spreadRadius: 2,
+                      blurRadius: 6,
+                      offset: Offset(0, 3),
+                    )
+                  ],
                   borderRadius: BorderRadius.circular(20),
                   image: DecorationImage(
                     fit: BoxFit.cover,
                     image: AssetImage(cards[index].recipeImage),
-                  )
-              ),
+                  )),
               child: Stack(
                 children: [
                   //Kind of recipe
@@ -57,18 +60,24 @@ class ListViewOfRecipeCards extends StatelessWidget {
                           sigmaX: 19.2,
                         ),
                         child: Container(
-                          padding: EdgeInsets.only(left: defaultPadding* 0.5, right: defaultPadding * 0.5, top: defaultPadding * 0.2, bottom: defaultPadding * 0.2),
+                          padding: EdgeInsets.only(
+                              left: defaultPadding * 0.5,
+                              right: defaultPadding * 0.5,
+                              top: defaultPadding * 0.2,
+                              bottom: defaultPadding * 0.2),
                           child: Text(
                             cards[index].recipeKind,
-                            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 14),
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14),
                           ),
                         ),
                       ),
                     ),
                   ),
                   SizedBox(
-                    width: size.width* 0.5,
-
+                    width: size.width * 0.5,
                   ),
                   //Detail Information
                   Align(
@@ -76,7 +85,7 @@ class ListViewOfRecipeCards extends StatelessWidget {
                     child: Container(
                       padding: EdgeInsets.all(defaultPadding * 0.45),
                       margin: EdgeInsets.only(bottom: defaultPadding),
-                      width: size.width* 0.45,
+                      width: size.width * 0.45,
                       height: size.height * 0.35 * 0.25,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20.0),
@@ -88,11 +97,14 @@ class ListViewOfRecipeCards extends StatelessWidget {
                           Align(
                             alignment: Alignment.topLeft,
                             child: Container(
-                              width: size.width* 0.45 * 0.6,
+                              width: size.width * 0.45 * 0.6,
                               height: size.height * 0.35 * 0.3 * 0.7,
                               child: Text(
                                 cards[index].recipeName,
-                                style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.w600),
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w600),
                               ),
                             ),
                           ),
@@ -102,19 +114,21 @@ class ListViewOfRecipeCards extends StatelessWidget {
                               right: -defaultPadding + 5,
                               child: IconButton(
                                 onPressed: () {},
-                                icon: Icon(Icons.bookmark_border_rounded, color: Colors.white, size: 18),
-                              )
-                          ),
+                                icon: Icon(Icons.bookmark_border_rounded,
+                                    color: Colors.white, size: 18),
+                              )),
 
                           //Time & Serving
                           Align(
                             alignment: Alignment.bottomLeft,
                             child: Text(
-                              cards[index].recipeCookTime + ' | ' + cards[index].recipeServingNumber.toString() + " " + AppLocalizations.of(context).serving,
+                              cards[index].recipeCookTime +
+                                  ' | ' +
+                                  cards[index].recipeServingNumber.toString() +
+                                  " " +
+                                  AppLocalizations.of(context).serving,
                               style: TextStyle(
-                                  color: Colors.blueGrey,
-                                  fontSize: 12
-                              ),
+                                  color: Colors.blueGrey, fontSize: 12),
                             ),
                           ),
                         ],
